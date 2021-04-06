a548284a08159c43fa4fe2c356d7e1a6cd70f2c9
Liv <78355864+OP119078@users.noreply.github.com>
2021-04-06 13:52:40 +0100

    Added JS for live data

---
+++ src/app.js
@@ -0,0 +1,43 @@
+function formatDate(date) {
+  
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let day = days[date.getDay()];
+
+  let dateNumber = date.getDate();
+
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  let month = months[date.getMonth()];
+
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+  
+  document.querySelector("#current-date").innerHTML = `${day} ${dateNumber} ${month}`
+  document.querySelector("#current-time").innerHTML = `${hour}:${minutes}`
+
+}
+
+function displayWeather(response) {
+    document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp)
+    document.querySelector("#description").innerHTML = response.data.weather[0].description;
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#wind-speed").innerHTML = Math.round((response.data.wind.speed) * 3.6);
+}
+
+let apiKey = "a57cca630b0e893126f37f33164019a3"
+let city = "London"
+let units = "metric"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`
+
+console.log(apiUrl)
+axios.get(apiUrl).then(displayWeather)
+
+let now = new Date();
+formatDate(now);
